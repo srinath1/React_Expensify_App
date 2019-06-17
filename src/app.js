e479493974eb1565f851e9a382813e2441b75339
srinath1 <srinath.erp@gmail.com>
2019-06-17 21:29:37 +0200

    the initial_1 commit

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+
 
 const jsx = (
   <Provider store={store}>
