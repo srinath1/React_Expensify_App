dcb120249e37d7d50c74818ba4eed5acb055d9ba
srinath1 <srinath.erp@gmail.com>
2019-06-18 20:31:12 +0200

    A css production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,51 @@
 const path = require('path');
+const ExtractTextWebpackPlugin=require('extract-text-webpack-plugin')
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSxtract=new ExtractTextWebpackPlugin('styles.css')
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use:CSSxtract.extract({
+            use:[
+                {
+loader:'css-loader',
+options:{
+  sourceMap:true
+}
+
+                },
+              {
+
+                loader:'sass-loader',
+                options:{
+                  sourceMap:true
+                }
+              }
+            ]
+        })
+      }]
+    },
+    plugins:[
+      CSSxtract
+
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
